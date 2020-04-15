5df12161b550e1b2205151eee5b30f1f49b7d376
Nilesh Paliwal <pnilesh120897@gmail.com>
2020-04-15 22:37:24 +0530

    Add files via upload

---
+++ new_financialAppData.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 14:27:18 2019
+
+@author: Nilesh
+"""
+
+ 
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+%matplotlib qt5
+  
+dataset = pd.read_csv('new_financialAppData.csv')
+
+
+#### Data Pre-Processing ####
+
+ 
+
+response = dataset["enrolled"]
+dataset = dataset.drop(columns="enrolled")
+
+ 
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(dataset, response,
+                                                    test_size = 0.2,
+                                                    random_state = 0)
+# Removing Identifiers
+
+
+train_identity = X_train['user']
+X_train = X_train.drop(columns = ['user'])
+test_identity = X_test['user']
+X_test = X_test.drop(columns = ['user'])
+
+# Feature Scaling
+
+#scaling and normalizing
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train2 = pd.DataFrame(sc_X.fit_transform(X_train))
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
+X_train2.columns = X_train.columns.values
+X_test2.columns = X_test.columns.values
+X_train2.index = X_train.index.values
+X_test2.index = X_test.index.values
+X_train = X_train2
+X_test = X_test2
+
+ 
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0, penalty = 'l1')
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+ 
+from sklearn.metrics import confusion_matrix, accuracy_score 
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred)
+ 
+
+df_cm = pd.DataFrame(cm, index = (0, 1), columns = (0, 1))
+plt.figure(figsize = (10,7))
+sn.set(font_scale=1.4)
+sn.heatmap(df_cm, annot=True, fmt='g')
+print("Test Data Accuracy: %0.4f" % accuracy_score(y_test, y_pred))
+
+ 
+ 
+final_results = pd.concat([y_test, test_identity], axis = 1).dropna()
+final_results['predicted_reach'] = y_pred
+final_results = final_results[['user', 'enrolled', 'predicted_reach']].reset_index(drop=True)
+
+
